bc192a49f893d5eda75f2a4ecc5b86e5f9f97d0b
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-04-21 13:57:11 +0700

    d2sqlite3 removed

--- source/app.d
+++ source/app.d
@@ -1,27 +1,20 @@
 import std.stdio;
-import d2sqlite3;
 import paper_plane_bot.grab;
 
 void main()
 {
-    import std.file: exists;
+    import std.file;
+    import std.datetime;
 
-    const string filename = "packages.sqlite3";
-    const bool isNeedNewDB = !filename.exists;
+    const string filename = "last_updated_mtime.txt";
 
-    auto db = Database(filename);
+    SysTime lastModified = Clock.currTime;
 
-    if(isNeedNewDB)
     {
-        writeln(`DB file `~filename~`is not found, creating new one`);
+        try
+            lastModified = filename.timeLastModified;
+        catch(FileException){} // file not found, will be created new one
 
-        db.run(`CREATE TABLE packages (
-                  name  TEXT PRIMARY KEY NOT NULL,
-                  updated TEXT NOT NULL
-                )`);
-
-        writeln(`Downloading packages list and filling out DB`);
-
-        auto packages = getPackagesList;
+        filename.write([]);
     }
 }
