a96b5b2cdef8a8bd3e159779043edd3ab5b4bdb3
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-04-22 11:06:55 +0700

    upsertPackages added

--- source/paper_plane_bot/db.d
+++ source/paper_plane_bot/db.d
@@ -8,6 +8,8 @@ private Statement stmnt_addChatId;
 private Statement stmnt_checkChatId;
 private Statement stmnt_getChatIds;
 private Statement stmnt_delChatId;
+private Statement stmnt_upsertPackage;
+private Statement stmnt_getPackageVersion;
 
 void openDb(string filename = "paper_plane_db.sqlite3")
 {
@@ -16,6 +18,11 @@ void openDb(string filename = "paper_plane_db.sqlite3")
     db.run(`CREATE TABLE IF NOT EXISTS chats (
                 chat_id INTEGER PRIMARY KEY NOT NULL,
                 descr TEXT
+            )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS packages (
+                name TEXT PRIMARY KEY NOT NULL,
+                version TEXT NOT NULL
             )`);
 
     stmnt_addChatId = db.prepare(
@@ -33,6 +40,15 @@ void openDb(string filename = "paper_plane_db.sqlite3")
 
     stmnt_delChatId = db.prepare(
         "DELETE FROM chats WHERE chat_id = :chat_id"
+    );
+
+    stmnt_upsertPackage = db.prepare(
+        "INSERT OR REPLACE INTO packages (name, version)
+         VALUES (:name, :version)"
+    );
+
+    stmnt_getPackageVersion = db.prepare(
+        "SELECT version FROM packages WHERE name = :name"
     );
 }
 
@@ -87,4 +103,48 @@ unittest
 
     delChatId(123);
     assert(getChatIds.length == 1);
+}
+
+import paper_plane_bot.grab: PackageDescr;
+
+/// Returns: packages with changed version since last check
+PackageDescr[] upsertPackages(PackageDescr[] pkgs)
+{
+    PackageDescr[] changed;
+
+    foreach(ref pkg; pkgs)
+    {
+        stmnt_getPackageVersion.bind(1, pkg.name);
+        auto res = stmnt_getPackageVersion.execute;
+
+        if(
+            res.empty || // new package
+            pkg.ver != res.front.peek!string(0) // version changed
+        )
+        {
+            stmnt_upsertPackage.inject(pkg.name, pkg.ver);
+            changed ~= pkg;
+        }
+
+        stmnt_getPackageVersion.reset;
+    }
+
+    return changed;
+}
+
+unittest
+{
+    openDb(":memory:");
+
+    PackageDescr pkg1 = {name: "test1", ver: "123"};
+    PackageDescr pkg2 = {name: "test2", ver: "456"};
+
+    auto r1 = upsertPackages([pkg1, pkg2]);
+    assert(r1.length == 2);
+
+    pkg2.ver = "888";
+
+    auto upsert1 = upsertPackages([pkg1, pkg2]);
+    assert(upsert1.length == 1);
+    assert(upsert1[0].ver == "888");
 }
